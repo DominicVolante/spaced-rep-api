ea0a9b6fd65c477a7840d25ba8bd53ae7a6379b0
Dominic Volante <dominicvolante@gmail.com>
2021-01-07 18:44:03 -0500

    working on deploy

--- migrations/004.do.seed_tables.sql
+++ migrations/004.do.seed_tables.sql
@@ -6,10 +6,9 @@ TRUNCATE
 INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
   (
-    1,
+    2,
     'guest',
     'Guest',
-    -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const knex = require('knex')
-const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config')
-
+const knex = require("knex");
+const app = require("./app");
+const { PORT, DATABASE_URL } = require("./config");
+const pg = require("pg");
+pg.defaults.ssl = true;
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: DATABASE_URL,
-})
+});
 
-app.set('db', db)
+app.set("db", db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
